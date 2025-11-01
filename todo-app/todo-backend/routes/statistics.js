e633a6672045b27ce17dc58281524aa28ab63b14
vilho4 <ville.j.holopainen@student.jyu.fi>
2025-11-01 11:36:48 +0200

    12.11 completed

--- todo-app/todo-backend/routes/statistics.js
+++ todo-app/todo-backend/routes/statistics.js
@@ -2,17 +2,10 @@ const express = require('express')
 const router = express.Router()
 const redis = require('../redis')
 
-const configs = require('../util/config')
-
-let visits = 0
-
-/* GET index data. */
 router.get('/', async (req, res) => {
-  visits++
-
-  res.send({
-    ...configs,
-    visits
+  const count = await redis.getAsync('added_todos')
+  res.json({
+    added_todos: Number(count) || 0
   })
 })
 
--- todo-app/todo-backend/routes/todos.js
+++ todo-app/todo-backend/routes/todos.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const { Todo } = require('../mongo')
 const router = express.Router()
+const redis = require('../redis')
 
 /* GET todos listing. */
 router.get('/', async (_, res) => {
@@ -10,10 +11,14 @@ router.get('/', async (_, res) => {
 
 /* POST todo to listing. */
 router.post('/', async (req, res) => {
+  console.log(req.body)
   const todo = await Todo.create({
     text: req.body.text,
-    done: false,
+    done: req.body.done || false,
   })
+  let count = await redis.getAsync('added_todos')
+  count = count ? Number(count) + 1 : 1
+  await redis.setAsync('added_todos', count)
   res.status(201).send(todo)
 })
 
